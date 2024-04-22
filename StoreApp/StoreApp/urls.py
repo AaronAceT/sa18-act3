445286da913d651239dbc28f065b273d58f98901
Aaron Turner <turneraaron67@gmail.com>
2024-04-22 02:20:21 -0500

    creating website but with errors

--- StoreApp/StoreApp/urls.py
+++ StoreApp/StoreApp/urls.py
@@ -17,6 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic.base import RedirectView
+from testapp import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('products/', views.index, name='index'),
+    path('products/<int:id>/', views.show, name='show'),
 ]
--- StoreApp/testapp/views.py
+++ StoreApp/testapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Product
+
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'myapp/index.html', {'products': products})
+
+def show(request, id):
+    product = get_object_or_404(Product, pk=id)
+    return render(request, 'myapp/show.html', {'product': product})
